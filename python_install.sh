72629fa4e81f96b2d78f10a6723be69998a1198a
wasnot <wasnot.apps@gmail.com>
2017-05-12 17:10:06 +0900

    fix readline for pyenv

--- python_install.sh
+++ python_install.sh
@@ -9,4 +9,8 @@ curl https://sdk.cloud.google.com | bash
 pyenv shell 2.7.13
 gcloud components install app-engine-python
 
+# readline fix
+# http://qiita.com/hidekuro/items/546a7945b0ce566a80ee
+ln -s /usr/local/opt/readline/lib/libreadline.dylib /usr/local/opt/readline/lib/libreadline.6.dylib
+
 echo "end!"
